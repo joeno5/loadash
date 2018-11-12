a356bc13a8d150d83ab1d4594e4c3c0d6273e683
joeno5 <joe.onepiece@gmail.com>
2018-11-12 10:55:56 +0800

    test

--- test.js
+++ test.js
@@ -12,7 +12,6 @@ setTimeout(function(){
     console.log("Hello World");
 },1);
 
-
 function foo() {
     // NOTE: don't ever do crazy long-running loops like this
     for (var i=0; i<=10000; i++) {
